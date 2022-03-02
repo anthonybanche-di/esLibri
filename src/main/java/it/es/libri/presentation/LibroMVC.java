57bfe94c50db5649c7ef2855cc9c1ba8a1e906fe
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-02 11:35:11 +0100

    Aggiunto_Controllo nel service

--- src/main/java/it/es/libri/presentation/LibroMVC.java
+++ src/main/java/it/es/libri/presentation/LibroMVC.java
@@ -21,67 +21,80 @@ import it.es.libri.service.LibroService;
 @Controller
 @RequestMapping("/listaLibri")
 public class LibroMVC {
-	
+
 	@Autowired
 	LibroService srv;
 
-	
-	
+
+
 	@GetMapping
 	public String getLibri(Libro l, Model m) {
-		
+
 		String titolo = "Lista Libri";
-		
+
 		List<Libro> libri = srv.getListaLibri();
-		
+
 		m.addAttribute("titolo",titolo);
 		m.addAttribute("libri", libri);
-		
+
 		return "libri2";
 	}
-	
+
 	@GetMapping("/updLibro/{id}")
 	public String updLibro(@PathVariable ("id") int id,Model m) {
-		
+
 		String titolo = "Modifica Libro";
 		m.addAttribute("titolo2", titolo);
-		
+
 		Libro modificare = srv.getLibroById(id);
-		
+
 		m.addAttribute("formLibro",modificare);
-	
+
 		return "updateLibro";
 	}
-	
+
 	@PostMapping("/updLibro")
 	public String updLibro(@ModelAttribute("libro") Libro l) {
 		srv.updLibro(l);
 		return "redirect:/listaLibri";
 	}
-	
+
 	@GetMapping("/delLibro/{id}")
 	public String delLibro(@PathVariable("id") int id) {
-		
+
 		try {
 			srv.deleteLibroById(id);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
+
+		return "redirect:/listaLibri";
+	}
+
+
+	@PostMapping("/saveLibro")
+	public String addLibro(Libro l) {
+		
+		
+		srv.addLibro(l);
+		
 		
 		return "redirect:/listaLibri";
 	}
-	
-	@PostMapping("/addLibro")
-	public String addLibro(@ModelAttribute("libro") Libro l,Model m ) {
-		
+
+
+
+	@GetMapping("/addLibro")
+	public String addLibro(Model m) {
+
 		String titolo = "Aggiungi Libro";
-		 m.addAttribute("titolo",titolo);
-		 
-		 m.addAttribute("newLibro", srv.addLibro(l));
-		 
-		 return "redirect:/listaLibri";
+		
+		m.addAttribute("titolo", titolo);
+		m.addAttribute("formLibro", new Libro()); 
+
+		return "addLibro";
 	}
-	
-	
-	
+
+
+
 }
--- src/main/java/it/es/libri/service/LibroService.java
+++ src/main/java/it/es/libri/service/LibroService.java
@@ -10,26 +10,34 @@ import it.es.libri.repository.LibroDB;
 
 @Service
 public class LibroService {
-	
+
 	@Autowired
 	LibroDB db;
-	
+
 	public List<Libro> getListaLibri() {
 		return db.findAll(); //Passacarte
 	}
-	
+
 	public Libro getLibroById(int id) {
 		return db.getById(id);
 	}
-	
+
 	public Libro addLibro(Libro l) {
-		return db.save(l);
+
+		if(!l.getTitolo().equals("")) {
+			if(Double.valueOf(l.getPrezzo()) != 0) {
+				if(Integer.valueOf(l.getPagine()) != 0) {
+					return db.save(l);
+				}
+			}
+		}
+		return null;
 	}
-	
+
 	public Libro updLibro(Libro l) {
 		return db.save(l);
 	}
-	
+
 	public void deleteLibroById(int id) {
 		db.deleteById(id);
 	}
