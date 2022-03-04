4f2e111a3cb7327bb3d284ff7323226e64ee0926
Anthony Banche (Data Inform SRL) <anthony.banche@datainform.it>
2022-03-04 14:46:04 +0100

    fix tests

--- prjSqlRelazioni/src/test/java/it/es/gestionale/EsTestCliente.java
+++ prjSqlRelazioni/src/test/java/it/es/gestionale/EsTestCliente.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GestionaleApplicationTests {
+class EsTestCliente {
 	@Autowired
 	EsempioDB db;
 
