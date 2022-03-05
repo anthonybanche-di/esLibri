6985971827cb59bdff66348555e9e5417ae05e3e
Giarry156 <mastertownz@gmail.com>
2022-03-05 12:29:54 +0100

    Query Tests done and passed.

--- prjSqlRelazioni/src/test/java/it/es/gestionale/EsTestArticolo.java
+++ prjSqlRelazioni/src/test/java/it/es/gestionale/EsTestArticolo.java
@@ -1,8 +1,11 @@
 package it.es.gestionale;
 
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +32,7 @@ class EsTestArticolo {
 	OrdineDB dbOr;
 
 	/*
-	 Elenca (mediante Sysout) gli utenti che hanno ordinato l'articolo
+	 Elenca (mediante Sysout) gli utenti che hanno ordinato l'articolo 4
 	*/
 	@Test
 	void esOrdiniImp() {
@@ -37,46 +40,108 @@ class EsTestArticolo {
 		List<DettaglioEntity> dettagli = dbDet.findAll();
 		
 		for(DettaglioEntity dettaglio : dettagli) {
-			if(dettaglio.getArticolo().getId() == 4) System.out.println(dettaglio.getOrdine().getClienteId());
+			if(dettaglio.getArticolo().getId() == 4) {
+				//System.out.println(dettaglio.getOrdine().getCliente().getId());
+				switch(dettaglio.getOrdine().getCliente().getId()) {
+				case 5: break;
+				case 6: break;
+				default: fail(); //identico con assertTrue(false);
+				break;
+				}
+			}
 		}
-		
-		assertTrue(false);
 	}
 
 	/*
 	 Conta e mostra il numero di ordini a sistema di ogni articolo
 	*/
 	@Test
+	@Transactional
 	void esCountOrdiniImp() {
-		assertTrue(false);
+		
+		List<ArticoloEntity> articoli = dbArt.findAll();
+		
+		for(ArticoloEntity articolo : articoli) {
+			switch(articolo.getId()) {
+			case 8:
+			case 9:
+				if(articolo.getDettagli().size() != 0) fail();
+				break;
+			case 6: 
+				if(articolo.getDettagli().size() != 1) fail();
+				break;
+			case 1:
+			case 4:
+			case 5:
+				if(articolo.getDettagli().size() != 2) fail();
+				break;
+			case 2:
+			case 3:
+				if(articolo.getDettagli().size() != 3) fail();
+				break;
+			case 7:
+				if(articolo.getDettagli().size() != 4) fail();
+				break;
+			default: fail();
+				break;
+			}
+			//System.out.println("ID: " + articolo.getId() + " count(ordine_id): " + articolo.getDettagli().size());
+		}	
 	}
 
 	/*
 	 Mostra la somma del totale venduto di un articolo
 	*/
 	@Test
+	@Transactional
 	void esSumVenduto() {
 
-		int idImpiegato=6;
-		assertTrue(false);
+		//int idImpiegato=6; ?
 		
+		List<DettaglioEntity> dettagli = dbDet.findAll();
+		double[] somme = new double[7];
+		double[] risultati = {4550, 616, 1800, 1600, 1400, 1752.50, 408};
+		
+		for(DettaglioEntity dettaglio : dettagli) {
+			somme[dettaglio.getArticolo().getId()-1] = somme[dettaglio.getArticolo().getId()-1] + (dettaglio.getQuantita()*dettaglio.getArticolo().getPrezzo());
+		}
+		
+		for(int i = 0; i<risultati.length; i++) {
+			//System.out.println("ID: " + (i+1) + " count(ordine_id): " + somme[i]);
+			if(somme[i] != risultati[i]) fail();
+		}
 	}
+		//assertTrue(false);
 
 	/*
 	 Mostra il cliente che ha ordinato il numero maggiore di articolo 2
 	*/
 	@Test
+	@Transactional
 	void esCountOrdiniAllImp() {
-		assertTrue(false);
+		
+		int[] quantita = new int[7];
+		int maxCID = 0, maxQ = 0;
+		
+		List<DettaglioEntity> dettagli = dbDet.findAll();
+		
+		for(DettaglioEntity dettaglio : dettagli) {
+			if(dettaglio.getArticolo().getId() == 2) {
+				quantita[dettaglio.getOrdine().getCliente().getId()-1] = quantita[dettaglio.getOrdine().getCliente().getId()-1] + dettaglio.getQuantita();
+			}
+		}
+		
+		for(int i=0; i<quantita.length; i++) {
+			//System.out.println("ID: " + (i+1) + " Quantita 2: " + quantita[i]);
+			if(quantita[i]>maxQ) {
+				maxCID = i+1;
+				maxQ = quantita[i];
+			}
+		}
+		
+		if(maxCID != 5 || maxQ != 10) fail();
+		
+		//assertTrue(false);
 		
 	}
-
-
-
-
-
-
-
-
-
 }
